199d339db31d5fa9548022c1c8a84be56cf5bea6
Nic Netzel and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-18 09:43:04 -0800

    Add page routes for full functionality

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/~~~~~~.php";
+    require_once __DIR__."/../src/ScrabbleGenerator.php";
 
     session_start();
 
@@ -8,6 +8,16 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
+
+    $app->get('/', function() use($app){
+        return $app['twig']->render('home.html.twig');
+    });
+
+    $app->get('/play', function() use($app){
+        $game = new ScrabbleGenerator();
+        $score = $game->getScore($_GET['scrabble']);
+        return $app['twig']->render('home.html.twig', array('scrabblePlay' => $score));
+    });
 
 
 
